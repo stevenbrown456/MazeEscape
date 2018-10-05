c00a0810cfabaa9b035652193ebbafb8cd2c87fe
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-10-05 15:10:21 +0100

    added tiles and score

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform followTarget;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        if (followTarget != null)
+        {
+            Vector3 newPosition = followTarget.position;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+	}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    //public variales in unity
+    public TextMesh displayText;
+
+    //Private variables can't be touched by other scripts 
+
+    private int currentValue = 0;
+
+    //update both the data value and the visual display
+
+    public void ChangeValue(int _toChange)
+
+
+    {
+
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+
+    }
+    //reset the score to zero
+
+    public void ResetScore()
+
+    {
+        currentValue = 0;
+        displayText.text = currentValue.ToString();
+
+    }
+	
+	
+	
+	
+}
